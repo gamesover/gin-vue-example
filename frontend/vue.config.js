4cead0780752bfbf924f77dd2892ca512830584b
gamesover <gamesover@users.noreply.github.com>
2020-10-25 03:48:03 +1030

    add frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devServer: {
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:9080',
+          ws: true,
+          changeOrigin: true
+        },
+        '^/foo': {
+          target: '<other_url>'
+        }
+      }
+    }
+  }
